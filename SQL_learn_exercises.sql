ef884400d249106c080938a955ca7f44674b68b5
Bohdan Dolishnii <dolishniibohdan@gmail.com>
2019-02-01 17:15:20 +0200

    Add new exercises

--- SQL_learn_exercises.sql
+++ SQL_learn_exercises.sql
@@ -66,7 +66,6 @@ FROM Product LEFT JOIN
 WHERE PC.speed >= 450
 GROUP BY maker
 
-
 -- 10. Find the printer models having the highest price. Result set: model, price.
 SELECT model, price
 FROM Printer
@@ -74,10 +73,237 @@ WHERE price = (SELECT MAX(price)
  FROM Printer)
  
  -- 11. Find out the average speed of PCs.
-select avg(speed)
-from PC
+SELECT AVG(speed)
+FROM PC
 
--- 12. 
+-- 12. Find out the average speed of the laptops priced over $1000.
+SELECT AVG(speed)
+FROM Laptop
+WHERE price > 1000
+
+-- 13. Find out the average speed of the PCs produced by maker A.
+SELECT AVG(PC.speed)
+FROM Product LEFT JOIN
+PC ON PC.model = Product.model
+WHERE Product.maker = 'A'
+
+-- 14. Get the makers who produce only one product type and more than one model. Output: maker, type.
+SELECT DISTINCT maker, MAX(type) as type
+FROM Product
+GROUP BY maker
+HAVING COUNT(distinct type) = 1 
+AND COUNT(model) > 1
+
+-- 15. Get hard drive capacities that are identical for two or more PCs. Result set: hd.
+SELECT hd
+FROM PC
+GROUP BY hd
+HAVING COUNT(hd) >= 2
+
+-- 16. Get pairs of PC models with identical speeds and the same RAM capacity. Each resulting pair should be displayed only once, i.e. (i, j) but not (j, i). 
+-- Result set: model with the bigger number, model with the smaller number, speed, and RAM.
+SELECT DISTINCT pc1.model, pc2.model, pc1.speed, pc1.ram
+FROM PC AS pc1 JOIN PC AS pc2
+ON pc1.speed = pc2.speed 
+AND pc1.ram = pc2.ram
+WHERE pc1.model > pc2.model
+
+-- 17. Get the laptop models that have a speed smaller than the speed of any PC. Result set: type, model, speed.
+SELECT DISTINCT pr.type AS Type, l.model as Model, l.speed
+FROM Product AS pr JOIN Laptop as l
+ON pr.model = l.model
+WHERE l.speed < ALL (SELECT speed
+ FROM PC)
+
+-- 18. Find the makers of the cheapest color printers.Result set: maker, price.
+SELECT DISTINCT Product.maker, Printer.price 
+FROM Product JOIN Printer 
+ON Product.model = Printer.model 
+WHERE Printer.color = 'y' 
+AND Printer.price = (SELECT MIN(price)
+ FROM Printer
+ WHERE color = 'y')
+
+-- 19. For each maker having models in the Laptop table, find out the average screen size of the laptops he produces. Result set: maker, average screen size.
+SELECT pr.maker, AVG(l.screen)
+FROM Product as pr 
+JOIN Laptop AS l 
+ON pr.model= l.model
+GROUP BY pr.maker
+
+-- 20. Find the makers producing at least three distinct models of PCs. Result set: maker, number of PC models.
+SELECT maker, COUNT(model)
+FROM Product
+WHERE type = 'PC'
+GROUP BY maker
+HAVING COUNT(model) >= 3
+
+-- 21. Find out the maximum PC price for each maker having models in the PC table. Result set: maker, maximum price.
+SELECT pr.maker, MAX(DISTINCT pc.price)
+FROM Product AS pr
+JOIN PC AS pc
+ON pr.model= pc.model
+GROUP BY pr.maker
+
+
+-- 22. For each value of PC speed that exceeds 600 MHz, find out the average price of PCs with identical speeds. Result set: speed, average price.
+SELECT speed, AVG(price)
+FROM PC
+WHERE speed > 600
+GROUP BY speed
+
+--23. Get the makers producing both PCs having a speed of 750 MHz or higher and laptops with a speed of 750 MHz or higher. Result set: maker
+SELECT DISTINCT maker
+FROM Product
+WHERE maker IN (SELECT DISTINCT maker 
+ FROM Product AS pr
+ JOIN PC as pc
+ ON pr.model = pc.model
+ WHERE pc.speed >= 750)
+AND maker IN (SELECT DISTINCT maker 
+ FROM Product AS pr 
+ JOIN Laptop AS l
+ ON pr.model = l.model
+ WHERE l.speed >= 750)
+
+--24. List the models of any type having the highest price of all products present in the database.
+WITH max
+AS
+(SELECT model, price FROM PC
+UNION 
+SELECT model, price FROM Laptop
+UNION 
+SELECT model, price FROM printer)
+
+SELECT model FROM max
+WHERE price = (SELECT MAX(price) FROM max)
+
+--25. Find the printer makers also producing PCs with the lowest RAM capacity and the highest processor speed of all PCs having the lowest RAM capacity. Result set: maker.
+select distinct maker
+from Product join PC
+on Product.model = PC.model
+where ram =
+(select min(ram)
+from PC)
+and speed =
+(select max(speed)
+from PC
+where ram =
+(select min(ram)
+from PC))
+and maker in
+(select maker
+from Product
+where type='Printer')
+
+-- 26. Find out the average price of PCs and laptops produced by maker A. Result set: one overall average price for all items.
+select avg(price)
+from
+(select price
+from Product join PC
+on Product.model = PC.model
+where maker = 'A'
+union all
+select price
+from Product join Laptop
+on Product.model = Laptop.model
+where maker='A'
+) as AVG_price
+
+--27. Find out the average hard disk drive capacity of PCs produced by makers who also manufacture printers. Result set: maker, average HDD capacity.
+SELECT pr.maker, AVG(pc.hd)
+FROM Product AS pr JOIN PC as pc 
+ON pr.model = pc.model
+WHERE maker IN (select DISTINCT maker
+from Product
+where type='Printer')
+GROUP BY maker
+
+
+-- 28. Using Product table, find out the number of makers who produce only one model.
+with total_count
+as
+(select maker
+from product 
+group by maker 
+having count(model) = 1)
+
+select count(maker)
+from total_count
+
+--29. Under the assumption that receipts of money (inc) and payouts (out) are registered not more than once a day for each collection point 
+-- [i.e. the primary key consists of (point, date)], 
+-- write a query displaying cash flow data (point, date, income, expense). Use Income_o and Outcome_o tables.
+select i.point, i.date, i.inc, o.out
+from Income_o i left join Outcome_o o
+on i.point = o.point
+and i.date = o.date
+union
+select o.point, o.date, i.inc, o.out
+from Outcome_o o left join Income_o i
+on o.point = i.point
+and o.date = i.date
+
+-- 30. Under the assumption that receipts of money (inc) and payouts (out) can be registered any number of times a day for each collection point 
+-- [i.e. the code column is the primary key], display a table with one corresponding row for each operating date of each collection point.
+-- Result set: point, date, total payout per day (out), total money intake per day (inc). Missing values are considered to be NULL.
+select i.point, i.date, o.out, i.inc
+from
+(select point, date, sum(inc) as inc
+from Income
+group by point, date) as i
+left join
+(select point, date, sum(out) as out
+from Outcome
+group by point, date) as o
+on i.point = o.point
+and i.date = o.date
+union
+select o.point, o.date, o.out, i.inc
+from
+(select point, date, sum(out) as out
+from Outcome
+group by point, date) as o
+left join
+(select point, date, sum(inc) as inc
+from Income
+group by point, date) as i
+on o.point = i.point
+and o.date = i.date
+
+-- 31. For ship classes with a gun caliber of 16 in. or more, display the class and the country.
+select class, country
+from Classes
+where bore >= 16.0
+
+-- 32. One of the characteristics of a ship is one-half the cube of the calibre of its main guns (mw). Determine the average.
+with total 
+as
+(select country, bore, name
+from Classes join Ships
+on Classes.class = Ships.class
+union
+select country, bore, ship
+from Classes join Outcomes
+on Classes.class = Outcomes.ship)
+
+select country, cast(round(avg(power(bore,3)*0.5),2) as numeric(10,2)) as weight from total
+group by country
+
+-- 33. Get the ships sunk in the North Atlantic battle. Result set: ship.
+select ship
+from Outcomes
+where result = 'sunk'
+and battle = 'North Atlantic'
+
+-- 34. In accordance with the Washington Naval Treaty concluded in the beginning of 1922, it was prohibited to build battle ships with a displacement of more than 35 thousand tons. 
+-- Get the ships violating this treaty (only consider ships for which the year of launch is known). List the names of the ships.
+select distinct name 
+from Classes as cl join Ships as sh 
+on cl.class = sh.class 
+where launched >= 1922 
+and displacement > 35000
+and type = 'bb'
 
 -- 35. Find models in the Product table consisting either of digits only or Latin letters (A-Z, case insensitive) only. Result set: model, type.
 select model, type
@@ -85,6 +311,64 @@ from Product
 where model not like '%[^0-9]%' or model not like '%[^a-z]%'
 or model not like '%[^A-Z]%'
 
+-- 36. List the names of lead ships in the database (including the Outcomes table).
+select name
+from Ships
+where name in
+(select class
+from Classes)
+
+union
+select ship
+from Outcomes
+where ship in
+(select class
+from Classes)
+
+-- 37. Find classes for which only one ship exists in the database (including the Outcomes table).
+with total_ship
+as
+(select cl.class, sh.name
+from Classes as cl join Ships as sh
+on cl.class = sh.class
+union
+select cl.class, o.ship as name
+from Classes as cl join Outcomes as o
+on cl.class = o.ship)
+
+select class 
+from total_ship 
+group by class 
+having count(class) = 1
+
+-- 38. Find countries that ever had classes of both battleships (‘bb’) and cruisers (‘bc’).
+select distinct country
+from Classes
+where type = 'bb'
+and country in
+(select distinct country
+from Classes
+where type = 'bc')
+
+-- 39. Find the ships that "survived for future battles"; that is, after being damaged in a battle, they participated in another one, which occurred later.
+select distinct o2.ship from
+(select ship, battle, result, date
+from Outcomes join Battles
+on Outcomes.battle = Battles.name
+where result='damaged'
+) as o1 join
+(select ship, battle, result, date
+from Outcomes join Battles
+on Outcomes.battle = Battles.name
+) as o2
+on o1.ship = o2.ship
+where o1.date < o2.date
+
+-- 40. For the ships in the Ships table that have at least 10 guns, get the class, name, and country.
+select cl.class, sh.name, cl.country
+from Ships as sh, Classes as cl
+where sh.class = cl.class
+and numGuns >= 10
 
 -- 41. For the PC in the PC table with the maximum code value, obtain all its characteristics (except for the code) and display them in two columns:
 -- - name of the characteristic (title of the corresponding column in the PC table);
@@ -99,34 +383,122 @@ from PC
 where code = (select max(code) from PC)
 
 union
-
 select  'model' as chr, cast(model as varchar) as value 
 from PC 
 where code = (select max(code) from PC)
 
 union
-
 select  'speed' as chr, cast(speed as varchar) as value 
 from PC 
 where code = (select max(code) from PC)
 
 union
-
 select  'ram' as chr, cast(ram as varchar) as value 
 from PC 
 where code = (select max(code) from PC)
 
 union
-
 select  'hd' as chr, cast(hd as varchar)  as value 
 from PC 
 where code = (select max(code) from PC)
 
 union
-
 select  'price' as chr, cast(price as varchar) as value 
 from PC 
 where code = (select max(code) from PC)
+
+-- 42. Find the names of ships sunk at battles, along with the names of the corresponding battles.
+select ship, battle
+from Outcomes
+where result = 'sunk'
+
+-- 43. Get the battles that occurred in years when no ships were launched into water.
+select name
+from Battles
+where year(date)
+not in
+(select launched
+from Ships
+where launched is not null)
+
+-- 44. Find all ship names beginning with the letter R.
+select name
+from Ships
+where name like 'R%'
+union
+select ship
+from Outcomes
+where ship like 'R%'
+
+-- 45. Find all ship names consisting of three or more words (e.g., King George V). Consider the words in ship names to be separated by single spaces, 
+-- and the ship names to have no leading or trailing spaces.
+select name
+from Ships
+where name like '% % %'
+union
+select ship
+from Outcomes
+where ship like '% % %'
+
+-- 46. For each ship that participated in the Battle of Guadalcanal, get its name, displacement, and the number of guns.
+select distinct ship, displacement, numguns
+from Classes left join Ships
+on classes.class = ships.class
+right join Outcomes
+on Classes.class = ship
+or ships.name = ship
+where battle = 'Guadalcanal'
+
+-- 47. Number the rows of the Product table as follows: makers in descending order of number of models produced by them (for manufacturers producing an equal number of models, 
+-- their names are sorted in ascending alphabetical order); model numbers in ascending order. Result set: row number as described above, manufacturer's name (maker), model.
+select count(*) num, t1.maker, t1.model
+from (select maker, model, c
+from Product
+join (select count(model) c, maker m
+from Product
+group by maker ) b1
+on maker = m) t1
+join
+(select maker, model, c
+from Product
+join (select count(model) c, maker m
+from Product
+group by maker ) b2
+on maker = m) t2
+on t2.c > t1.c
+or t2.c=t1.c and t2.maker<t1.maker
+or t2.c=t1.c and t2.maker=t1.maker and t2.model <= t1.model
+group by t1.maker, t1.model
+order by 1
+
+-- 48. Find the ship classes having at least one ship sunk in battles.
+select distinct Classes.class
+from Classes, Ships, Outcomes
+where Classes.class = Ships.class
+and Ships.name = Outcomes.ship
+and Outcomes.result = 'sunk'
+union
+select distinct class
+from Classes, Outcomes
+where Classes.class = Outcomes.ship
+and Outcomes.result = 'sunk'
+
+-- 49. Find the names of the ships having a gun caliber of 16 inches (including ships in the Outcomes table).
+select name
+from Ships, Classes
+where Ships.class = Classes.class
+and bore = 16
+union
+select ship
+from Outcomes, Classes
+where Outcomes.ship = Classes.class
+and bore = 16
+
+-- 50. Find the battles in which Kongo-class ships from the Ships table were engaged.
+select battle
+from Outcomes, Ships
+where Outcomes.ship = Ships.name
+and Ships.class = 'Kongo'
 
 -- 51. Find the names of the ships with the largest number of guns among all ships having the same displacement (including ships in the Outcomes table).
 select s.name
@@ -137,10 +509,20 @@ from Classes
 where Classes.displacement = c.displacement)
 
 union
-
 select o.ship
 from Outcomes as o, Classes as c
 where o.ship = c.class
 and c.numGuns = (select max(numGuns)
 from Classes
 where Classes.displacement = c.displacement)
+
+-- 53. Determine the names of all ships in the Ships table that can be a Japanese battleship having at least nine main guns with 
+-- a caliber of less than 19 inches and a displacement of not more than 65 000 tons.
+select name 
+from Ships, Classes
+where Ships.class = Classes.class 
+and (country = 'Japan' or country is null) 
+and (numGuns >= 9 or numGuns is null)     
+and (bore < 19 or bore is null) 
+and (displacement <= 65000 or displacement is null) 
+and (type = 'bb' or type is null)
