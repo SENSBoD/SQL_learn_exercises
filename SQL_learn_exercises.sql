00f30964776b86f4d7d9d895fb965d50833bf796
Bohdan Dolishnii <dolishniibohdan@gmail.com>
2019-09-16 15:15:10 +0300

    Change SQL_learn_exercises.sql

--- SQL_learn_exercises.sql
+++ SQL_learn_exercises.sql
@@ -765,4 +765,11 @@ FULL JOIN (
  ) AS III 
  ON III.point = I.point
  
- -- 63. 
+ -- 63. Find the names of different passengers that ever travelled more than once occupying seats with the same number.
+SELECT p.name
+FROM Passenger p JOIN Pass_in_trip pt
+ON p.ID_psg = pt.ID_psg
+GROUP BY p.name, p.id_psg
+HAVING COUNT(DISTINCT pt.place) < COUNT(*)
+
+-- 64. 
